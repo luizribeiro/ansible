a3a4d5d6d59b2a16ba6a2ddb6d03cb9016bbc118
Luiz Ribeiro <luizribeiro@gmail.com>
2020-01-12 01:21:45 -0500

    [backup] Changed restic forget settings

--- roles/backup/templates/backup.sh
+++ roles/backup/templates/backup.sh
@@ -6,7 +6,7 @@ export RESTIC_PASSWORD="{{ secret_restic_password }}"
 {
   restic backup {{ backup_paths | join (" ") }}
   restic check
-  restic forget 
+  restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12
 } 2>&1 | tee ~/.restic-output
 
 touch ~/.restic-lastrun
