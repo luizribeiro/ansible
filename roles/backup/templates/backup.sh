9b87f66c9e004b45b560a6f83d45e3ee8caa5217
Luiz Ribeiro <luizribeiro@gmail.com>
2020-01-16 00:03:08 -0500

    [backup] Allow for excluding certain patterns from backups

--- roles/backup/templates/backup.sh
+++ roles/backup/templates/backup.sh
@@ -4,7 +4,7 @@ export RESTIC_REPOSITORY="{{ restic_backup_repo }}"
 export RESTIC_PASSWORD="{{ secret_restic_password }}"
 
 {
-  restic backup {{ backup_paths | join (" ") }}
+  restic backup --exclude-file backup-excludes.txt {{ backup_paths | join (" ") }}
   restic check
   restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12
 } 2>&1 | tee /var/log/backup.log
