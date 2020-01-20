a0ad63a8dd2d5da43657b388b9038f17f81c8a92
Luiz Ribeiro <luizribeiro@gmail.com>
2020-01-20 13:53:36 -0500

    [backup] Output tweaks to backup.sh

--- roles/backup/templates/backup.sh
+++ roles/backup/templates/backup.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 export RESTIC_REPOSITORY="{{ restic_backup_repo }}"
 export RESTIC_PASSWORD="{{ secret_restic_password }}"
 
+DATE=$(command -v gdate date | head -1)
+
 {
+  echo ">>> ===================================================================="
+  echo -n ">>> $($DATE): Backing up..."
   restic backup --exclude-file backup-excludes.txt {{ backup_paths | join (" ") }}
+  echo ">>> DONE"
+  echo
+
+  echo ">>> $($DATE): Checking backups..."
   restic check
+  echo ">>> DONE"
+  echo
+
+  echo ">>> $($DATE): Cleaning up old backups..."
   restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12
-} 2>&1 | tee /var/log/backup.log
+  echo ">>> DONE"
+  echo
+} 2>&1 | tee -a /var/log/backup.log
