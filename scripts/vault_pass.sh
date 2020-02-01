9522c1798753b1517b5a2c8d734363fc78f7c587
Luiz Ribeiro <luizribeiro@gmail.com>
2020-01-31 20:22:19 -0500

    Allow vault_pass.sh to be called from anywhere

--- scripts/vault_pass.sh
+++ scripts/vault_pass.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-gpg --batch --use-agent --decrypt vault_pass.gpg
+OLD_DIR="$(pwd)"
+cd "$(dirname "$0")" || exit 1
+gpg --batch --use-agent --decrypt ../vault_pass.gpg
+cd "$OLD_DIR" || exit 1
