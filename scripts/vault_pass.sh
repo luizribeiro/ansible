e058119f9971a3bb2d26f5cd6f05409368d0dfee
Luiz Ribeiro <luizribeiro@gmail.com>
2020-02-16 22:31:28 -0500

    [scripts] Make vault_pass.sh decrypt vault silently

--- scripts/vault_pass.sh
+++ scripts/vault_pass.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 OLD_DIR="$(pwd)"
 cd "$(dirname "$0")" || exit 1
-gpg --batch --use-agent --decrypt ../vault_pass.gpg
+gpg --quiet --batch --use-agent --decrypt ../vault_pass.gpg
 cd "$OLD_DIR" || exit 1
