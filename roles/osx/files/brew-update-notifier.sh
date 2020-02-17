90a71f945b2d843a75c652db97a24f8d7ede75e9
Luiz Ribeiro <luizribeiro@gmail.com>
2020-02-17 16:38:07 -0500

    [osx] Only notify about outdated formulae once a day

--- roles/osx/files/brew-update-notifier.sh
+++ roles/osx/files/brew-update-notifier.sh
@@ -2,14 +2,28 @@
 
 BREW=$(command -v brew)
 TERMINAL_NOTIFIER=$(command -v terminal-notifier)
+STATUS_FILE="$HOME/.local/.brew-last-notified"
 
 $BREW update > /dev/null 2>&1
 
-outdated=$(($BREW outdated --quiet && $BREW cask outdated --quiet) | sort -u)
+outdated=$( ($BREW outdated --quiet && $BREW cask outdated --quiet) | sort -u)
 pinned=$($BREW list --pinned)
 updatable=$(comm -1 -3 <(echo "$pinned") <(echo "$outdated"))
 
 if [ -n "$updatable" ] && [ -e "$TERMINAL_NOTIFIER" ]; then
+  current_time=$(date +%s)
+  if [ -f "$STATUS_FILE" ]; then
+    last_notif=$(stat -f "%Y" "$STATUS_FILE")
+  else
+    last_notif=0
+  fi
+
+  if (( last_notif < (current_time - (60 * 60 * 24)) )); then
+    # limit to one notification per day
+    exit
+  fi
+
+  touch "$STATUS_FILE"
   $TERMINAL_NOTIFIER -sender com.apple.Terminal \
     -title "Homebrew Updates Available" \
     -subtitle "The following formulae are outdated:" \
