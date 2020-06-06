f8b934262635743492f7292d54ff938cd22137f7
Luiz Ribeiro <luizribeiro@gmail.com>
2020-06-06 17:02:41 -0400

    Tweaks to vault_pass.sh

--- scripts/vault_pass.sh
+++ scripts/vault_pass.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+
+set -eo pipefail
 OLD_DIR="$(pwd)"
-cd "$(dirname "$0")" || exit 1
-gpg --quiet --batch --use-agent --decrypt ../vault_pass.gpg
+cd "$(dirname "$0")/.." || exit 1
+
+gpg --quiet --batch --use-agent --decrypt ./vault_pass.gpg
+
 cd "$OLD_DIR" || exit 1
