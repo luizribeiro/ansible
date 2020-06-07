8d226f322a68f7ef1cd35f738a0aa40a88be90e6
Luiz Ribeiro <luizribeiro@gmail.com>
2020-06-07 15:46:13 -0400

    Fixed initssh and allowed it to run with host name

--- scripts/src/common.sh
+++ scripts/src/common.sh
@@ -1,15 +1,30 @@
 ANSIBLE_INVENTORY=$(ansible-inventory --list)
 
-list_hosts() {
+_list_hosts_impl() {
   local group="$1"
-  local hosts=($(echo "$ANSIBLE_INVENTORY" | jq -r ".$group.hosts // [] | .[]"))
+  local hosts=($(echo "$ANSIBLE_INVENTORY" | jq -r ".[\"$group\"].hosts // [] | .[]"))
   local children=($(
-    echo "$ANSIBLE_INVENTORY" | jq -r ".$group.children // [] | .[]"
+    echo "$ANSIBLE_INVENTORY" | jq -r ".[\"$group\"].children // [] | .[]"
   ))
   for child in "${children[@]}"; do
     local children_hosts=($(list_hosts "$child"))
     hosts=("${hosts[@]}" "${children_hosts[@]}")
   done
+
+  echo "${hosts[@]}"
+}
+
+list_hosts() {
+  local group="$1"
+  local all_hosts=($(
+    echo "$ANSIBLE_INVENTORY" | jq -r "._meta.hostvars | keys // [] | .[]"
+  ))
+  local hosts=($(_list_hosts_impl "$1"))
+
+  if [[ "${all_hosts[@]}" =~ "${group}" ]]; then
+    # group is actually a host, so add it to the list of hosts
+    hosts=("${hosts[@]}" "$group")
+  fi
 
   echo "${hosts[@]}"
 }
@@ -23,4 +38,10 @@ get_user() {
   local host="$1"
   echo "$ANSIBLE_INVENTORY" \
     | jq -r "._meta.hostvars[\"$host\"].ansible_user // \"$USER\""
+}
+
+get_host() {
+  local host="$1"
+  echo "$ANSIBLE_INVENTORY" \
+    | jq -r "._meta.hostvars[\"$host\"].ansible_host // \"$host\""
 }
