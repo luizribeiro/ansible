618aee6059787544733460e2b0809bb8b3dd1af3
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-17 10:34:16 -0400

    [tests] Basic firewall tests

--- roles/server/molecule/default/tests/test_server.py
+++ roles/server/molecule/default/tests/test_server.py
@@ -21,3 +21,29 @@ def test_monit_config(host: Host) -> None:
     with host.sudo():
         output = host.run("monit -t -c /etc/monitrc")
         assert output.rc == 0
+
+
+def test_firewall_drops_by_default(host: Host) -> None:
+    with host.sudo():
+        assert host.iptables.rules("filter", "INPUT") == [
+            "-P INPUT DROP",
+            "-A INPUT -j ufw-before-logging-input",
+            "-A INPUT -j ufw-before-input",
+            "-A INPUT -j ufw-after-input",
+            "-A INPUT -j ufw-after-logging-input",
+            "-A INPUT -j ufw-reject-input",
+            "-A INPUT -j ufw-track-input",
+        ]
+
+        expected_input_rules = {
+            "-N ufw-user-input",
+            "-A ufw-user-input -p tcp -m tcp --dport 22 -m conntrack"
+            + " --ctstate NEW -m recent --set --name DEFAULT"
+            + " --mask 255.255.255.255 --rsource",
+            "-A ufw-user-input -p tcp -m tcp --dport 22 -m conntrack"
+            + " --ctstate NEW -m recent --update --seconds 30 --hitcount 6"
+            + " --name DEFAULT --mask 255.255.255.255 --rsource -j ufw-user-limit",
+            "-A ufw-user-input -p tcp -m tcp --dport 22 -j ufw-user-limit-accept",
+        }
+        input_rules = set(host.iptables.rules("filter", "ufw-user-input"))
+        assert expected_input_rules & input_rules == expected_input_rules
