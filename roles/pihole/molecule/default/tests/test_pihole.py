3c7bb550e09285da3c708c1630fd79c07e2acfbd
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-21 15:28:04 -0400

    [pihole/tests] Added tests for permissions on setupVars.conf

--- roles/pihole/molecule/default/tests/test_pihole.py
+++ roles/pihole/molecule/default/tests/test_pihole.py
@@ -1,5 +1,6 @@
 import os
 
+from pyexpect import expect
 from testinfra.host import Host
 from testinfra.utils.ansible_runner import AnsibleRunner
 
@@ -10,3 +11,18 @@ testinfra_hosts = AnsibleRunner(os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts
 def test_pihole_is_installed(host: Host) -> None:
     assert host.package("pi-hole-server").is_installed
     assert host.package("pi-hole-ftl").is_installed
+
+
+def test_setupvars_conf_permissions(host: Host) -> None:
+    with host.sudo():
+        setupvars_conf = host.file("/etc/pihole/setupVars.conf")
+    expect(setupvars_conf.is_file).is_true()
+    expect(setupvars_conf.user).equals("pihole")
+    expect(setupvars_conf.group).equals("pihole")
+    expect(oct(setupvars_conf.mode)).equals("0o640")
+
+
+def test_setupvars_conf_can_be_read_by_http_user(host: Host) -> None:
+    with host.sudo():
+        getfacl = host.run("getfacl /etc/pihole/setupVars.conf")
+    expect(getfacl.stdout.split("\n")).to_contain("user:http:r--")
