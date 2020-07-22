b367df3ef5c54e9640ac640aa7e3dcd78e51e9cc
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-21 21:40:53 -0400

    [pihole/tests] Added test coverage for resolving domains (blocked or not)

--- roles/pihole/molecule/default/tests/test_pihole.py
+++ roles/pihole/molecule/default/tests/test_pihole.py
@@ -20,3 +20,26 @@ def test_setupvars_conf_permissions(host: Host) -> None:
     expect(setupvars_conf.user).equals("pihole")
     expect(setupvars_conf.group).equals("pihole")
     expect(oct(setupvars_conf.mode)).equals("0o644")
+
+
+SIMPLE_IP_ADDRESS_REGEXP = r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
+
+
+def _resolve(host: Host, domain: str) -> str:
+    dig = host.run(f"dig +short @127.0.0.1 {domain}")
+    return str(dig.stdout.split("\n")[-2])
+
+
+def test_dns_resolving(host: Host) -> None:
+    ip_address = _resolve(host, "www.google.com")
+    expect(ip_address).matches(SIMPLE_IP_ADDRESS_REGEXP)
+
+
+def test_dns_resolving_for_a_blocked_domain(host: Host) -> None:
+    ip_address = _resolve(host, "app-measurement.com")
+    expect(ip_address).equals("0.0.0.0")
+
+
+def test_dns_resolving_for_a_whitelisted_domain(host: Host) -> None:
+    ip_address = _resolve(host, "graph.instagram.com")
+    expect(ip_address).matches(SIMPLE_IP_ADDRESS_REGEXP)
