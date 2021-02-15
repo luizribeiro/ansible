54f2d98e706489c5c0533ece14e4034deb9e1682
Luiz Ribeiro <luizribeiro@gmail.com>
2021-02-15 13:47:37 -0600

    [CI] Make vault_pass.sh exit with 0 on GitHub Actions

--- scripts/vault_pass.sh
+++ scripts/vault_pass.sh
@@ -4,4 +4,9 @@ set -eo pipefail
 OLD_DIR="$(pwd)"
 cd "$(dirname "$0")/.." || exit 1
 
+if [ -n "$GITHUB_ACTIONS" ]; then
+  # github actions don't have a vault setup
+  exit 0
+fi
+
 gpg --quiet --batch --use-agent --decrypt ./vault_pass.gpg
