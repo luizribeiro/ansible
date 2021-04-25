19877b09479e34bee781286fe12f044ceb3f2b11
Luiz Ribeiro <luizribeiro@gmail.com>
2021-04-25 10:40:19 -0500

    [pihole] Fixed issue with slowness on Big Sur

--- roles/pihole/molecule/default/tests/test_pihole.py
+++ roles/pihole/molecule/default/tests/test_pihole.py
@@ -1,4 +1,6 @@
 import os
+import re
+from typing import Optional, Tuple
 
 from pyexpect import expect
 from testinfra.host import Host
@@ -25,21 +27,35 @@ def test_setupvars_conf_permissions(host: Host) -> None:
 SIMPLE_IP_ADDRESS_REGEXP = r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
 
 
-def _resolve(host: Host, domain: str) -> str:
-    dig = host.run(f"dig +short @127.0.0.1 {domain}")
-    return str(dig.stdout.split("\n")[-2])
+def _resolve(host: Host, domain: str) -> Tuple[str, Optional[str]]:
+    dig_full = host.run(f"dig @127.0.0.1 {domain}")
+    status_search = re.search(r"status: ([A-Z]+)", dig_full.stdout)
+    if status_search is None:
+        raise Exception("Could not parse dig response")
+    status = status_search.group(1)
+
+    dig_short = host.run(f"dig +short @127.0.0.1 {domain}")
+    try:
+        ip_address = str(dig_short.stdout.split("\n")[-2])
+    except IndexError:
+        ip_address = None
+
+    return (status, ip_address)
 
 
 def test_dns_resolving(host: Host) -> None:
-    ip_address = _resolve(host, "www.google.com")
+    status, ip_address = _resolve(host, "www.google.com")
+    expect(status).equals("NOERROR")
     expect(ip_address).matches(SIMPLE_IP_ADDRESS_REGEXP)
 
 
 def test_dns_resolving_for_a_blocked_domain(host: Host) -> None:
-    ip_address = _resolve(host, "app-measurement.com")
-    expect(ip_address).equals("0.0.0.0")
+    status, ip_address = _resolve(host, "app-measurement.com")
+    expect(status).equals("NXDOMAIN")
+    expect(ip_address).equals(None)
 
 
 def test_dns_resolving_for_a_whitelisted_domain(host: Host) -> None:
-    ip_address = _resolve(host, "graph.instagram.com")
+    status, ip_address = _resolve(host, "graph.instagram.com")
+    expect(status).equals("NOERROR")
     expect(ip_address).matches(SIMPLE_IP_ADDRESS_REGEXP)
